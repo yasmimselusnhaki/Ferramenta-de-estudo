dbbb0f19e83abec82deb53c4f70b02e274fefc68
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-07-30 08:26:50 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,15 @@ criaCartao(
      'O que é Bhaskara?',
      'A fórmula de Bhaskara é um cálculo matemático utilizado para determinar as raízes de uma função de segundo grau por meio de seus coeficientes.'
 )
+
+criaCartao(
+    'Física',
+    'Como mede a velocidade em Física?',
+    'Em física, a velocidade é medida calculando-se a distância percorrida por um objeto e o tempo que leva para percorrer essa distância, sendo a fórmula mais comum a da velocidade média: \(v_{m}=\Delta s/\Delta t\), onde \(v_{m}\) é a velocidade média, \(\Delta s\) é o deslocamento (ou distância) e \(\Delta t\) é o intervalo de tempo.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que se faz em Biologia?',
+    'A Biologia é a ciência que estuda os seres vivos, sua estrutura, funcionamento, evolução e relação com o meio ambiente.'
+)
