212353004873e1bb4e55fbd1050b4dbcf6b63a22
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-07-30 08:05:30 -0300

    modificação

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Médica Veterinária',
+    'O que é uma Médica Veterinária?',
+    'Um médico veterinário atua na saúde e bem-estar de animais, diagnosticando, prevenindo e tratando doenças, além de cuidar da segurança alimentar e da sustentabilidade ambiental.'
+)
+
+criaCartao (
+    'Zootecnia',
+    'O que é Zootecnia?',
+    'A zootecnia é a ciência que estuda a criação e o manejo de animais domésticos e selvagens, com o objetivo de otimizar a produção animal e garantir o bem-estar dos animais.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é a capital Irlanda?',
+    'A capital da Irlanda é o Dublin'
+)
+
+criaCartao(
+     'Matemática',
+     'O que é Bhaskara?',
+     'A fórmula de Bhaskara é um cálculo matemático utilizado para determinar as raízes de uma função de segundo grau por meio de seus coeficientes.'
+)
