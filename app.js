3e1ed14495c9f8f8ac6b8bef10eee45ca9c75397
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-08-13 07:46:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-    
+     <div class="cartao__conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>${pergunta}</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>${respota}</p>
+                    </div>
+                </div>
+                `
+
+    container.apeendChild(cartao)
 }
