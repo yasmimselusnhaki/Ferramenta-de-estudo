454dbfa96f00a0971e5b9708979b6ef57db2fd60
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-08-13 08:21:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function criaCartao(categoria, pergunta, resposta) {
     <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
-    <p>${respota}</p>
+    <p>${resposta}</p>
     </div>
     </div>
     `
