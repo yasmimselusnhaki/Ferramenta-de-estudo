889f54285c01d003fd3f6e779b5b959e173798ed
yasmimselusnhaki <yasmim.selusnhaki@escola.pr.gov.br>
2025-08-13 07:35:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function criaCartao(categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+    let container = document.getElementByIdI('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    
 }
